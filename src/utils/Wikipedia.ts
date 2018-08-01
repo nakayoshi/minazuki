099826b048cd5207a7b20335db3a8db734a6495f
neet <n33t5hin@gmail.com>
2018-08-02 01:57:16 +0900

    Fix to upper case

---
+++ src/utils/Wikipedia.ts
@@ -0,0 +1,30 @@
+import WikiJS from 'wikijs';
+
+export default class Wikipedia  {
+
+  protected cleint = WikiJS({
+    apiUrl: 'https://ja.wikipedia.org/w/api.php',
+  });
+
+  /**
+   * Fuzzy search an Wikipedia article
+   * @param keyword Query string
+   * @return Result Page object, I wonder why WikiJS didn't export it
+   */
+  public async search (keyword: string): Promise<any> {
+    try {
+      const { results } = await this.cleint.search(keyword);
+
+      if (!results.length) {
+        return null;
+      }
+
+      const page = await this.cleint.page(results[0]);
+
+      return page;
+
+    } catch (e) {
+      return null;
+    }
+  }
+}
