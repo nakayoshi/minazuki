385ad303638b93aee862c64b5c2644fc7ac6e8a1
neet <n33t5hin@gmail.com>
2018-08-03 02:43:12 +0900

    Update snapshot

--- src/utils/__tests__/verticalize.spec.ts
+++ src/utils/__tests__/verticalize.spec.ts
@@ -2,14 +2,11 @@ import verticalize from '../verticalize';
 
 describe('verticalize', () => {
   it ('verticalizes text', () => {
-    const text = `
-      Discord（ディスコード）とは、Windows、macOS、Android、iOS、Linux、Webブラウザで動作する、
-      ゲーマー向けの音声通話・VoIPフリーソフトウェア。
-      2018年5月16日時点で、ユーザー数が1億3千万人を上回っている。
-    `;
+    const text = 'aaa\nbbb\nccc';
 
     const result = verticalize(text);
+    const resultJson = JSON.stringify(result);
 
-    expect(result).toMatchSnapshot();
+    expect(resultJson).toMatchSnapshot();
   });
 });
