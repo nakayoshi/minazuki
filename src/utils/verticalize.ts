ac2e12aea14cf37a8bc9f93869a9d986e5ef80ae
neet <n33t5hin@gmail.com>
2018-09-25 01:18:02 +0900

    Improve valicalizer

--- src/utils/verticalize.ts
+++ src/utils/verticalize.ts
@@ -1,7 +1,119 @@
-export const toFullWidth = (s: string) => {
-  return s.replace(/[A-Za-z0-9]/g, (s) => String.fromCharCode(s.charCodeAt(0) + 0xFEE0));
+const verticalAndHorizontalCharactorMap: { targets: string[], vertical: string }[] = [
+  {
+    targets: ['｜'],
+    vertical: 'ー',
+  },
+  {
+    targets: ['（', '('],
+    vertical: '︵',
+  },
+  {
+    targets: [')', '）'],
+    vertical: '︶',
+  },
+  {
+    targets: ['「', '｢'],
+    vertical: '﹁',
+  },
+  {
+    targets: ['｣', '」'],
+    vertical: '﹂',
+  },
+  {
+    targets: ['『'],
+    vertical: '﹃',
+  },
+  {
+    targets: ['』'],
+    vertical: '﹄',
+  },
+  {
+    targets: ['[', '［'],
+    vertical: '﹇',
+  },
+  {
+    targets: [']', '］'],
+    vertical: '﹈',
+  },
+  {
+    targets: ['{', '｛'],
+    vertical: '︷',
+  },
+  {
+    targets: ['}', '｝'],
+    vertical: '︸',
+  },
+  {
+    targets: ['〔', '❲'],
+    vertical: '︹',
+  },
+  {
+    targets: ['〕', '❳'],
+    vertical: '︺',
+  },
+  {
+    targets: ['〈', '〈'],
+    vertical: '︿',
+  },
+  {
+    targets: ['〉', '〉'],
+    vertical: '﹀',
+  },
+  {
+    targets: ['《', '⟪'],
+    vertical: '︽',
+  },
+  {
+    targets: ['》', '⟫'],
+    vertical: '︾',
+  },
+  {
+    targets: ['【'],
+    vertical: '︻',
+  },
+  {
+    targets: ['】'],
+    vertical: '︼',
+  },
+  {
+    targets: ['〖'],
+    vertical: '︗',
+  },
+  {
+    targets: ['〗'],
+    vertical: '︘',
+  },
+];
+
+/**
+ * Transform `a-z` to `ａ-ｚ` style
+ * @param text text to transform
+ * @return transformed text
+ */
+export const transformLatinToFullWidth = (text: string) => {
+  return text.replace(/[A-Za-z0-9]/g, (text) => String.fromCharCode(text.charCodeAt(0) + 0xFEE0));
 };
 
+/**
+ * Transform specific charactors to mapped vartical style charactor
+ * @param text text to transfrom
+ * @return transformed text
+ */
+export const transformHorizontalToVerticalUnicode = (text: string) => {
+  verticalAndHorizontalCharactorMap.forEach(({ targets, vertical }) => {
+    targets.forEach((target) => {
+      text = text.replace(target, vertical);
+    });
+  });
+
+  return text;
+};
+
+/**
+ * Verticalize Japanese text from horizontal
+ * @param text Target text to transform
+ * @return Varticalized text
+ */
 export default function verticalize (text: string): string {
   const lines = text.trim().split('\n').map((line) => line.split('')).reverse();
   const whiteSpace = '　';
@@ -21,8 +133,9 @@ export default function verticalize (text: string): string {
 
   // Avoid trimming
   verticalizedText = verticalizedText.replace(/^\s+?/, '.' + whiteSpace);
-  // Convert alphabet to full width
-  verticalizedText = toFullWidth(verticalizedText);
+
+  // Transforming to appropriate unicodes
+  verticalizedText = transformHorizontalToVerticalUnicode(transformLatinToFullWidth(verticalizedText));
 
   return verticalizedText;
 }
