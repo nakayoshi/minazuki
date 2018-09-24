a40a75b4af0fac31acd45f86c606faa48b321439
neet <n33t5hin@gmail.com>
2018-09-25 01:23:21 +0900

    Add unicode-specific verticalization test

--- src/utils/__tests__/verticalize.spec.ts
+++ src/utils/__tests__/verticalize.spec.ts
@@ -1,10 +1,17 @@
 import verticalize from '../verticalize';
 
 describe('verticalize', () => {
-  it ('verticalizes text', () => {
+  it ('verticalizes latin text', () => {
     const text = 'aaa\nbbb\nccc';
     const result = verticalize(text);
 
     expect(result).toEqual('ｃｂａ\nｃｂａ\nｃｂａ\n');
+  });
+
+  it ('verticalizes specific unicode charactors', () => {
+    const text = '彼は「あーあ」と言った';
+    const result = verticalize(text);
+
+    expect(result).toEqual('彼\nは\n﹁\nあ\n｜\nあ\n﹂\nと\n言\nっ\nた\n');
   });
 });
