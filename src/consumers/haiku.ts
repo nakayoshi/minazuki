1832ade5abe83b2eff0f1c2cca132af7b12efa60
Neetshin <n33t5hin@gmail.com>
2019-11-29 15:35:25 +0900

    Keep instance for haiku

--- src/consumers/haiku.ts
+++ src/consumers/haiku.ts
@@ -3,10 +3,11 @@ import { Consumer } from '.';
 import { filterNotBot } from '../operators';
 import { MatsuoBasho } from '../utils/matsuo-basho';
 
+const dict = path.resolve(require.resolve('kuromoji'), '../../dict');
+const basho = new MatsuoBasho([5, 7, 5], dict);
+
 export const haiku: Consumer = async context =>
   context.message$.pipe(filterNotBot).subscribe(async message => {
-    const dict = path.resolve(require.resolve('kuromoji'), '../../dict');
-    const basho = new MatsuoBasho([5, 7, 5], dict);
     const match = await basho.findHaiku(message.content);
 
     if (!match || !match.length) {
