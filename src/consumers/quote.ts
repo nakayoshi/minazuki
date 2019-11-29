f4f1c0ea94699df5c9175ad197c8b475b954d3ed
Neetshin <n33t5hin@gmail.com>
2019-11-29 14:59:35 +0900

    Add better quotation feature

--- src/consumers/quote.ts
+++ src/consumers/quote.ts
@@ -1,7 +1,28 @@
+import { ClientUser, TextChannel, User } from 'discord.js';
 import { Consumer } from '.';
+import { Context } from '../context';
 import { filterNotBot, filterStartsWith } from '../operators';
 import { interpretMessageLike } from '../utils/message-like';
 import { toQuotation } from '../utils/to-quotation';
+
+const fetchWebhookOrCreate = async (context: Context, channel: TextChannel) => {
+  const webhook = await channel
+    .fetchWebhooks()
+    .then(webhooks =>
+      webhooks.find(
+        ({ owner }) =>
+          owner instanceof User &&
+          context.client.user instanceof ClientUser &&
+          owner.id === context.client.user.id,
+      ),
+    );
+
+  if (webhook) {
+    return webhook;
+  }
+
+  return channel.createWebhook('minazuki');
+};
 
 export const quote: Consumer = context =>
   context.message$
@@ -12,25 +33,39 @@ export const quote: Consumer = context =>
     .subscribe(async message => {
       const { channel, content } = message;
 
+      if (!(channel instanceof TextChannel)) {
+        return;
+      }
+
       // tslint:disable-next-line no-floating-promises
       channel.startTyping();
 
+      // Match quote message
       const match = /\>\s(?<messageLike>[^\s]+)/.exec(content);
-
       if (!match || !match.groups || !match.groups.messageLike) {
         return channel.stopTyping(true);
       }
 
+      // Find message from message-like
       const matchedMessage = await interpretMessageLike(
         match.groups.messageLike,
         message,
       );
-
       if (!matchedMessage) {
         return channel.stopTyping(true);
       }
 
-      const embed = toQuotation(matchedMessage);
-      channel.stopTyping(true);
-      return channel.send(embed);
+      // Delete original message
+      if (message.deletable) {
+        await message.delete();
+      }
+
+      const webhook = await fetchWebhookOrCreate(context, channel);
+      await webhook.send(undefined, {
+        username: message.author.username,
+        avatarURL: message.author.avatarURL(),
+        embeds: [toQuotation(matchedMessage)],
+      });
+
+      return channel.stopTyping(true);
     });
--- src/utils/to-quotation.ts
+++ src/utils/to-quotation.ts
@@ -2,11 +2,7 @@ import { Message, MessageEmbed } from 'discord.js';
 
 export const toQuotation = (message: Message) => {
   return new MessageEmbed()
-    .setAuthor(
-      message.author.tag,
-      message.author.avatarURL({ size: 16 }),
-      message.url,
-    )
+    .setAuthor(message.author.tag, message.author.avatarURL(), message.url)
     .setDescription(message.content)
     .setTimestamp(message.createdTimestamp);
 };
